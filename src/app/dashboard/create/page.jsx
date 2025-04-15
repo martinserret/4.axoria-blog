efa628265b73c82c06d0553c08abf1ce9e8c37bf
Martin010 <martin.serret47@gmail.com>
2025-04-15 21:22:40 +0200

    Formulaire de mise en ligne: - Création du formulaire - Création de l'input Title - Ajout de la classe utilitaire f-label dans le css

--- src/app/dashboard/create/page.jsx
+++ src/app/dashboard/create/page.jsx
@@ -1,7 +1,32 @@
+"use client";
+
 export default function page() {
+  function handleSubmit(e){
+    e.preventDefault();
+
+
+  }
+
   return (
     <main className="u-main-container bg-white p-7 mt-32 mb-44">
+      <h1 className="text-4xl mb-4">Write an article ✍️</h1>
 
+      <form 
+        onSubmit={handleSubmit}
+        className="pb-6"
+      >
+        <label htmlFor="title" className="f-label">Title</label>
+        <input
+          type="text"
+          name="title"
+          className="shadow border border-gray-300 rounded w-full p-3 mb-7 text-gray-700 focus:outline-2 focus:-outline-offset-2 focus:outline-gray-400"
+          id="title"
+          placeholder="Title"
+          required
+        >
+
+        </input>
+      </form>
     </main>
   );
 }
