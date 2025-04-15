3c628026875f893060db1fa50f6fda83c1ea529f
Martin010 <martin.serret47@gmail.com>
2025-04-15 16:02:13 +0200

    Page accueil : - données statiques - cartes - classes utilitaires dans le css

---
+++ src/app/dashboard/create/page.jsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <main className="u-main-container bg-white p-7 mt-32 mb-44">
+
+    </main>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,62 @@
+import Link from "next/link";
+
+const posts = [
+  {
+    author: "John Doe",
+    title: "Why it's hard to be no one"
+  },
+  {
+    author: "Dwight Schrute",
+    title: "Top 5 best beets"
+  },
+  {
+    author: "Cleon the first",
+    title: "10 advices to become a great emperor"
+  },
+];
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="pt-44">Axoria Blog</h1>
+    <div className="u-main-container u-padding-content-container">
+      <h1 className="t-main-title">Stay up to date with AXORIA</h1>
+      <p className="t-main-subtitle">Tech news and useful knowledge</p>
+
+      <p className="text-md text-zinc-900">Latest articles</p>
+      <ul className="u-articles-grid">
+        {posts.map((post, id) => (
+          <li
+            className="rounded-sm shadow-md border border-transparent hover:shadow-xl hover:border-zinc-300"
+            key={id}
+          >
+            <div className="pt-5 px-5 pb-7">
+              <div className="flex items-baseline gap-x-4 text-xs">
+                <time
+                  dateTime={new Date().toISOString()}
+                  className="text-gray-500 text-sm"
+                > 
+                  {new Date().toLocaleDateString("en-EN", { year: "numeric", month: "long", day: "numeric" })}
+                </time>
+
+                <Link 
+                  href={`/categories/author/${post.author}`}
+                  className="ml-auto text-base text-gray-700 hover:text-gray-600 whitespace-nowrap truncate"
+                >
+                  {post.author}
+                </Link>
+              </div>
+              <Link 
+                href={`/article/${post.title}`}
+                className="inline-block mt-6 text-xl font-semibold text-zinc-800 hover:text-zinc-600"
+              >
+                {post.title}
+              </Link>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+// <time dateTime={new Date().toISOString()}>  : meilleur élément pour le SEO et donc pour l'indexation des pages (meilleur lecture des crawlers)
+// {new Date().toLocaleDateString("en-EN", { year: "numeric", month: "long", day: "numeric" })} : il suffit de remplacer par fr-FR pour l'avoir en français
