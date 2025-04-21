c129c49ba263bcd4d70b212cd6475c7d198c771e
Martin010 <martin.serret47@gmail.com>
2025-04-21 17:15:30 +0200

    Enrichissement de getPost et affichage des tags

--- src/app/article/[slug]/page.jsx
+++ src/app/article/[slug]/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { getPost } from "@/lib/serverMethods/blog/postMethods";
 
 export default async function page({ params }) {
@@ -7,6 +8,17 @@ export default async function page({ params }) {
   return (
     <main className="u-main-container u-padding-content-container">
       <h1 className="text-4xl mb-3">{post.title}</h1>
+      <p className="mb-6">
+        {post.tags.map(tag => (
+          <Link
+            href={`categories/tag/${tag.slug}`}
+            key={tag.slug}
+            className="mr-4 underline"
+          >
+            #{tag.name}
+          </Link>
+        ))}
+      </p>
       <p>{post.markdownArticle}</p>
     </main>
   );
--- src/lib/serverMethods/blog/postMethods.js
+++ src/lib/serverMethods/blog/postMethods.js
@@ -1,12 +1,12 @@
 import { connectToDB } from "@/lib/utils/db/connectToDB";
 import { Post } from "@/lib/models/post";
-
+import { Tag } from "@/lib/models/tag";
 
 export async function getPosts() {
   try {
     await connectToDB();
 
-    // récupère tous les articles existants
+    // Récupère tous les posts existants
     const posts = await Post.find({});
     return posts;
   } catch(error) {
@@ -19,10 +19,19 @@ export async function getPost(slug) {
   try {
     await connectToDB();
 
-    const post = await Post.findOne({ slug });
+    // Récupère un post via son slug
+    const post = await Post.findOne({ slug }).populate({
+      path: "tags",
+      select: "name slug"
+    });
     return post;
   } catch(error) {
     console.error("Error while fetch a post", error);
     throw new Error("Failed to fetch post");
   }
 }
+
+
+// Post.findOne({ slug }).populate : populate permet d'enrichir notre résultat. Dans notre cas, on utilise :
+//  - path: "tags" : référence à notre collection tags dans mongodb
+//  - select: "name slug" : champs de notre collection que l'on souhaite ajouter dans l'objet de retour (ici le name et le slug)
