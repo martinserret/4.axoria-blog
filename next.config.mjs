b8de18b9413043f10d62b36989397a294dd05792
Martin010 <martin.serret47@gmail.com>
2025-04-25 14:48:37 +0200

    Création de la configuration du nom de domaine pour les requêtes des images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "axoria-blog-pz.b-cdn.net",
+      pathname: "/**"
+    }]
+  }
+};
 
 export default nextConfig;
+
+// Ici on configure le nom de domaine vers lequel Next.js va faire des requêtes pour les images : 
+// le protocol est https, le nom de domaine est axoria-blog-pz.b-cdn.net qui provient du pull zone de Bunny CDN et le pathname est /** (toutes les images)
