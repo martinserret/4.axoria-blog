b9bf50fcf743c817d98a0eb3d82f62bbf5e413e0
Martin010 <martin.serret47@gmail.com>
2025-04-28 15:41:13 +0200

    UI/UX de la page "Categories"

--- src/app/categories/page.jsx
+++ src/app/categories/page.jsx
@@ -1,11 +1,28 @@
+import Link from "next/link";
 import { getTags } from "@/lib/serverMethods/blog/tagMethods";
 
 export default async function page() {
   const tags = await getTags();
-  console.log(tags, "tagsConsole");
-  
   
   return (
-    <div>page</div>
+    <main className="u-main-container u-padding-content-container">
+      <h1 className="t-main-title">All categories</h1>
+      <p className="t-main-subtitle">Find articles sorted by category tag</p>
+
+      <ul className="u-articles-grid">
+        {tags.length > 0 ? (
+          tags.map(tag => (
+            <li key={tag._id} className="bg-gray-100 border border-gray-300 rounded shadow-md">
+              <Link href={`/categories/tag/${tag.slug}`} className="flex items-baseline p-4 ">
+                <span className=" text-lg font-semibold underline">#{tag.name}</span>
+                <span className="ml-auto">{tag.postCount}</span>
+              </Link>
+            </li>
+          ))
+        ) : (
+          <li>No categories found</li>
+        )}
+      </ul>
+    </main>
   );
 }
