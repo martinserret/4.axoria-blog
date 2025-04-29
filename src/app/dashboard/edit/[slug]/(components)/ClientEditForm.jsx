e51b6c20c6d690eb55fe0192ab601b64c18d26f2
Martin010 <martin.serret47@gmail.com>
2025-04-29 09:16:10 +0200

    Création de l'UI de la page ClientEditForm : - Identique à la page create - Les champs "title" et "markdownArticle" sont pré-rempli avec les données initiale du post - Le useState "tags" contient tous les tags du post

--- src/app/dashboard/edit/[slug]/(components)/ClientEditForm.jsx
+++ src/app/dashboard/edit/[slug]/(components)/ClientEditForm.jsx
@@ -1,7 +1,211 @@
+"use client";
+
+import { useState, useRef } from "react";
+import { useRouter } from "next/navigation";
+import { addPost } from "@/lib/serverActions/blog/postServerActions";
+
 export default function ClientEditForm({ post }) {
-  console.log(post, "ClientEditFormPost");
-  
+  const [tags, setTags] = useState(post.tags.map(tag => tag.name)); // On initialise useState avec les tags du post
+  const tagInputRef = useRef(null);
+  const submitButtonRef = useRef(null);
+  const serverValidationText = useRef(null);
+  const router = useRouter();
+
+  const imgUploadValidationText = useRef(null);
+
+
+  function handleAddTag() {
+    const newTag = tagInputRef.current.value.trim().toLowerCase();
+
+    if(newTag !== "" && !tags.includes(newTag) && tags.length < 5) {
+      setTags([...tags, newTag]);
+      tagInputRef.current.value = "";
+    }
+  }
+
+  function handleRemoveTag(tagToRemove) {
+    setTags(tags.filter(tag => tag !== tagToRemove));
+  }
+
+  function handleEnterOnTagInput(e) {
+    if(e.key === "Enter") {
+      e.preventDefault();
+      handleAddTag();
+    }
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    formData.set("tags", JSON.stringify(tags));
+
+    serverValidationText.current.textContent = ""; // Réinitialise le texte de serveur validation
+    submitButtonRef.current.textContent = "Updating Post..."; // Indique dans le bouton que la sauvegarde est en cours
+    submitButtonRef.current.disabled = true; // Evite le spam click
+
+    try {
+      const result = await addPost(formData);
+
+      if(result.success) {
+        submitButtonRef.current.textContent = "Post Updated ✅";
+
+        let countdown = 3;
+        serverValidationText.current.textContent = `Redirecting in ${countdown}...`;
+        const interval = setInterval(() => {
+          countdown -= 1;
+          serverValidationText.current.textContent = `Redirecting in ${countdown}...`;
+
+          if(countdown === 0) {
+            clearInterval(interval);
+            router.push(`/article/${result.slug}`);
+          }
+        }, 1000);
+      }
+    } catch(error) {
+      serverValidationText.current.textContent = `${error.message}`; // Affichage de l'erreur
+      submitButtonRef.current.textContent = "Submit"; // Réinitialise le texte du bouton
+      submitButtonRef.current.disabled = false; // Evite le spam click
+    }
+  }
+
+  function handleFileChange(e) {
+    const file = e.target.files[0];
+    const validImageTypes = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+
+    if(!validImageTypes.includes(file.type)) {
+      imgUploadValidationText.current.textContent = "Please upload a valid image (jpeg, jpg, png, webp)"; // Si l'image n'est pas valide, on affiche un message d'erreur
+      e.target.value = "";
+    } else {
+      imgUploadValidationText.current.textContent = ""; // Réinitialise le texte de validation
+    }
+
+    // On charge l'image pour vérifier ses dimensions
+    const img = new Image();
+    img.addEventListener("load", checkImgSizeOnLoad);
+
+    function checkImgSizeOnLoad() {
+      if(img.width > 1280 || img.height > 720) {
+        imgUploadValidationText.current.textContent = "Image exceeds 1280x720 dimensions"; // Si l'image n'a pas une dimension valide, on affiche un message d'erreur
+        e.target.value = "";
+        URL.revokeObjectURL(img.src); // Libère la mémoire
+        return;
+      } else {
+        imgUploadValidationText.current.textContent = ""; // Réinitialise le texte de validation
+        URL.revokeObjectURL(img.src); // Libère la mémoire
+      }
+    }
+
+    img.src = URL.createObjectURL(file); // Charge un objet une image et ses caractéristiques
+  }
+
   return (
-    <div>ClientEditForm</div>
+    <main className="u-main-container bg-white p-7 mt-32 mb-44">
+      <h1 className="text-4xl mb-4">Edit your article ✍️</h1>
+
+      <form 
+        onSubmit={handleSubmit}
+        className="pb-6"
+      >
+        {/* Title */}
+        <label htmlFor="title" className="f-label">Title</label>
+        <input
+          type="text"
+          name="title"
+          className="shadow border border-gray-300 rounded w-full p-3 mb-7 text-gray-700 focus:outline-gray-400"
+          id="title"
+          placeholder="Title"
+          required
+          defaultValue={post.title} // On initialise le champ avec le titre du post
+        />
+
+        {/* Image */}
+        <label htmlFor="coverImage" className="f-label">Cover image (1280x720 for best quality, or less)</label>
+        <input 
+          type="file" 
+          name="coverImage"
+          className="file:mr-5 file:p-3 file:rounded file:border-0 file:font-medium file:bg-indigo-500 file:text-white file:cursor-pointer w-full text-gray-700 hover:file:bg-indigo-700 mb-2"
+          id="coverImage"
+          required
+          placeholder="Article's cover image"
+          onChange={handleFileChange}
+        />
+        <p ref={imgUploadValidationText} className="text-red-700 mb-7"></p>
+
+        {/* Tags */}
+        <div className="mb-10">
+          <label className="f-label" htmlFor="tag">Add a tag(s) (optional, max: 5)</label>
+          <div className="flex">
+            <input 
+              type="text" 
+              className="shadow border border-gray-300 rounded p-3 text-gray-700 focus:outline-slate-400"
+              id="tag"
+              placeholder="Add a tag"
+              ref={tagInputRef}
+              onKeyDown={handleEnterOnTagInput}
+            />
+            <button
+              className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold p-4 rounded mx-4 cursor-pointer"
+              onClick={handleAddTag}
+              type="button"
+            >
+              Add
+            </button>
+            <div className="flex items-center grow whitespace-nowrap overflow-y-auto shadow border-rounded px-3">
+              {tags.map(tag => (
+                <span 
+                  key={tag}
+                  className="inline-block whitespace-nowrap bg-gray-200 text-gray-700 rounded-full px-3 py-1 text-sm font-semibold mr-2"
+                >
+                  {tag}
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveTag(tag)}
+                    className="text-red-500 ml-2 cursor-pointer"
+                  >
+                    &times;
+                  </button>
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* Markdown article */}
+        <label htmlFor="markdownArticle" className="f-label">Write your article using markdown (do not repeat the title)</label>
+        <a 
+          href="https://www.markdownguide.org/cheat-sheet/" 
+          target="_blank"
+          className="block mb-4 text-blue-600"
+        > How to use markdown syntax ?</a>
+        <textarea 
+          name="markdownArticle" 
+          id="markdownArticle"
+          required
+          className="min-h-44 border-gray-300 shadow appearance-none border rounded w-full p-8 text-gray-700 mb-4 focus:outline-slate-400"
+          placeholder="Write the content of your article"
+          defaultValue={post.markdownArticle} // On initialise le champ avec le contenu du post
+        ></textarea>
+
+        {/* Submit button */}
+        <button
+          ref={submitButtonRef}
+          className="min-w-44 bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-3 px-4 rounded mb-4 cursor-pointer"
+        >
+          Submit
+        </button>
+        <p
+          ref={serverValidationText}
+        ></p>
+      </form>
+    </main>
   );
 }
+
+// FormData() : objet JS permettant de stocker les valeurs des inputs d'un formulaire, notamment les données complexes comme
+// les images, les vidéos, les zip, etc., de manière optimisée. Un objet est automatiquement créé à partir de ces inputs. 
+
+
+// <button type="button"></button> : il est important de spécifier type="button" lorsque'on ajoute un bouton dans le formulaire dont l'objectif n'est pas de retourner (submit) le formulaire
+// mais qui réalise une autre action dans le formulaire (comme ici ajouter un tag). type peut aussi prendre la valeur "submit" si on souhaite qu'il submit le formulaire (comportement par défaut de type donc pas la peine
+// de le préciser si l'objectif du bouton est de submit le formulaire)
